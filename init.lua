626a0ae6d663fbf992531cc019f7c74345d62fbb
iyvysotskiy <iyvysotskiy@fintechiq.ru>
2026-06-22 19:08:39 +0700

    switch from vim.pack

--- init.lua
+++ init.lua
@@ -1,18 +1,3 @@
--- bootstrap lazy.nvim
---local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
---
---if not vim.loop.fs_stat(lazypath) then
---  vim.fn.system({
---    "git",
---    "clone",
---    "--filter=blob:none",
---    "https://github.com/folke/lazy.nvim.git",
---    lazypath,
---  })
---end
-
-vim.opt.rtp:prepend(lazypath)
-
 -- basic settings
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
